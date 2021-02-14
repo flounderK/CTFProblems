a168ab1a0de9e6fb031a3b205c052ce7f59df2dd
Clif Wolfe <clifwolfe1@gmail.com>
2021-02-14 17:16:31 -0500

    Added challenge listener

---
+++ challenge-listener.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import subprocess
+import socket
+import os
+import argparse
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("program", type=str)
+parser.add_argument("port")
+args = parser.parse_args()
+
+while 1:
+    os.system(f"socat TCP-LISTEN:{args.port},tcpwrap=script,reuseaddr,fork EXEC:'{args.program}',stderr,pty,rawer")
