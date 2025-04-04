f7fede41404ab25e26d7606175a32750b0f6f1f8
Clif Wolfe <clifwolfe1@gmail.com>
2025-04-04 18:31:44 -0400

    less verbose fuzz

--- pwn/Prints/fuzz.sh
+++ pwn/Prints/fuzz.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while [ 1 ]; do
-	dd if=/dev/urandom of=temp bs=1 count=48;
-	cat temp <(echo "quit") | ./prints
+	dd if=/dev/urandom of=temp bs=1 count=48 2>/dev/null
+	cat temp <(echo "quit") | ./prints >/dev/null
 	A=$?
 	if [ $A != 0 ]; then
 		break
